81b210362297c77b09b8e86baed57593096cd2ff
Bohdan Tkachenko <techbohdan@gmail.com>
2023-11-21 00:39:26 -0600

    Implemented Meal Detail View, Meal Detail Fetching and caching Meal Details

---
+++ Dessert Recipes/Views/MealDetailView.swift
@@ -0,0 +1,20 @@
+//
+//  MealDetailView.swift
+//  Dessert Recipes
+//
+//  Created by Bohdan Tkachenko on 11/20/23.
+//
+
+import SwiftUI
+
+struct MealDetailView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+struct MealDetailView_Previews: PreviewProvider {
+    static var previews: some View {
+        MealDetailView()
+    }
+}
