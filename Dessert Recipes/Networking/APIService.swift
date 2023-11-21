9fa459d4b686048a91c6e27e0c2c3b18223c45b2
Bohdan Tkachenko <techbohdan@gmail.com>
2023-11-20 22:21:28 -0600

    Added APIServer to fetch all meals, implemented Error handling, and tested fetching on UI

---
+++ Dessert Recipes/Networking/APIService.swift
@@ -0,0 +1,8 @@
+//
+//  APIService.swift
+//  Dessert Recipes
+//
+//  Created by Bohdan Tkachenko on 11/20/23.
+//
+
+import Foundation
---
+++ Dessert Recipes/Networking/MealFetcher.swift
@@ -0,0 +1,8 @@
+//
+//  MealFetcher.swift
+//  Dessert Recipes
+//
+//  Created by Bohdan Tkachenko on 11/20/23.
+//
+
+import Foundation
