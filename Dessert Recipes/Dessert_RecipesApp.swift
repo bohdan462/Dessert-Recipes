f9c22fb37af113c39d591db43a7798ebd4ed09ec
Bohdan Tkachenko <techbohdan@gmail.com>
2023-11-20 20:41:59 -0600

    Initial commit

---
+++ Dessert Recipes/Dessert_RecipesApp.swift
@@ -0,0 +1,17 @@
+//
+//  Dessert_RecipesApp.swift
+//  Dessert Recipes
+//
+//  Created by Bohdan Tkachenko on 11/20/23.
+//
+
+import SwiftUI
+
+@main
+struct Dessert_RecipesApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
